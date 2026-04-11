a866ea804730deed1960924086161d827049c39d
vaibhav <da-vaibhav@users.noreply.github.com>
2026-04-11 18:55:20 +0530

    clean-up old

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -2,8 +2,6 @@ import { Toaster } from "@26f3861b-5297-4e7c-92fa-6d4e397ff7fe/ui/components/son
 import { HeadContent, Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
-import Header from "@/components/header";
-import { ThemeProvider } from "@/components/theme-provider";
 
 import "../index.css";
 
@@ -34,18 +32,10 @@ function RootComponent() {
   return (
     <>
       <HeadContent />
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        disableTransitionOnChange
-        storageKey="vite-ui-theme"
-      >
-        <div className="grid grid-rows-[auto_1fr] h-svh">
-          <Header />
+      <div className="grid grid-rows-[auto_1fr] h-svh">
           <Outlet />
         </div>
-        <Toaster richColors />
-      </ThemeProvider>
+      <Toaster richColors />
       <TanStackRouterDevtools position="bottom-left" />
     </>
   );
